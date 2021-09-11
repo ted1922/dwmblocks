5757b98e17d1786d48ebacdeb2ed4cca06d611a7
ted1922 <59594230+ted1922@users.noreply.github.com>
2021-09-11 17:11:09 +1000

    signal calls, thanks to lukesmithxyz for the instructions

--- blocks.h
+++ blocks.h
@@ -1,9 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
+
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%a %d %b %R'",					        5,		0},
+	{"vol:", "~/.config/scripts/volume_get.sh",				0,		10},
+	{"", "date '+%a %d %b %R'",					        1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
